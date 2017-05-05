2c7112360a54c6981bfd25b4540310da354f048f
James Jarvis <jamespjarvis@gmail.com>
2017-05-05 11:12:44 -0400

    sweet ui changes

--- public/js/app.js
+++ public/js/app.js
@@ -4,42 +4,93 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress-filled');
 const toggle = player.querySelector('.toggle');
 const fullscreen = player.querySelector('.fullscreen');
+const volume = player.querySelector('.volume-toggle');
+const volumeRange = player.querySelector(`input[name='volume']`);
 
 const skipButtons = player.querySelectorAll('button[data-skip]');
 const ranges = player.querySelectorAll(`input[type='range']`);
 
-function togglePlay() {
-    video[video.paused ? 'play' : 'pause']();
-}
+const videoListItems = document.querySelectorAll('.list-item');
 
-function toggleFullscreen(e) {
-    if (document.webkitFullscreenElement) {
-        document.webkitExitFullscreen();
-    } else {
-        player.webkitRequestFullscreen();
+videoListItems.forEach(item => item.addEventListener('click', handleListClick));
+
+// update video player source
+function handleListClick(e) {
+  // update source
+  video.src = this.dataset.video;
+  video.currentTime = 0;
+  video.play();
+
+  // add active class to current video
+  videoListItems.forEach(item => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
     }
+  });
+  this.classList.add('active');
 }
 
+// toggle video play state
+function togglePlay() {
+  video[video.paused ? 'play' : 'pause']();
+}
+
+// toggle fullscreen video player
+function toggleFullscreen(e) {
+  if (document.webkitFullscreenElement) {
+    document.webkitExitFullscreen();
+    fullscreen.textContent = '';
+  } else {
+    player.webkitRequestFullscreen();
+    fullscreen.textContent = '✕';
+  }
+}
+
+// update volume
+function updateVolumeRange(level) {
+  volumeRange.value = level;
+}
+
+// change volume icon based on level
+// TODO
+function toggleVolume() {
+  if (this.dataset.level === 'full') {
+    this.dataset.level = 'mute';
+    this.innerHTML = '&#128263;';
+    video.volume = 0;
+  } else {
+    this.dataset.level = 'full';
+    this.innerHTML = '&#128266;';
+    video.volume = 1;
+  }
+  updateVolumeRange(video.volume);
+}
+
+// update play / pause butotn based on video state
 function updateButton() {
-    toggle.textContent = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = this.paused ? '►' : '❚ ❚';
 }
 
+// skip forward / back
 function skip() {
-    video.currentTime += parseFloat(this.dataset.skip);
-    console.log('skip');
+  video.currentTime += parseFloat(this.dataset.skip);
+  console.log('skip');
 }
 
+// update volume and / or playback rate
 function handleRangeUpdate() {
-    video[this.name] = this.value;
+  video[this.name] = this.value;
 }
 
+// update ui progress bar based on current time and duration
 function handleProgressUpdate() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
+// change play position
 function scrub(e) {
-    video.currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
 }
 
 video.addEventListener('click', togglePlay);
@@ -49,6 +100,7 @@ video.addEventListener('timeupdate', handleProgressUpdate);
 
 toggle.addEventListener('click', togglePlay);
 fullscreen.addEventListener('click', toggleFullscreen);
+volume.addEventListener('click', toggleVolume);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
@@ -59,4 +111,3 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
-progress.addEventListener('mouseleave', () => mousedown = false);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/video', (req, res, next) => {
-    res.render('video');
+  fs.readdir(path.resolve(__dirname, '../public/videos'), (err, files) => {
+    if (err) return next(err);
+    files = files.map(file =>  `videos/${file}`);
+    res.render('video', { files });
+  });
 });
 
 module.exports = router;
